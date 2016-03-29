862dd583a1961c604b443470f527e7c4c1280afc
codeurfou <selim.tavux@gmail.com>
2016-03-29 03:14:07 +0200

    Tested and functional (For EN and FR) with the actual "jasper-dev" branch.

--- wiki.py
+++ wiki.py
@@ -1,18 +1,17 @@
 #Written by Jake Schultz
 #TODO Add more lang support, limit number of results returned
 import re
-from urllib2 import Request, urlopen, URLError
+from urllib2 import Request, urlopen, URLError, quote
+import urllib
 import json
-
 from jasper import plugin
 
-WORDS = [self.gettext("WIKI"), self.gettext("WICKY")]
-
-PRIORITY = 1
-
 class WikiPlugin(plugin.SpeechHandlerPlugin):
-	def get_phrases(self)
-	    return WORDS
+	def get_priority(self):
+            return 1
+
+	def get_phrases(self):
+	    return [self.gettext("WIKI"), self.gettext("WICKY")]
 
 	def handle(self, text, mic):
 	    try:
@@ -21,16 +20,18 @@ class WikiPlugin(plugin.SpeechHandlerPlugin):
                 lang = 'en'
 	    
 	    # method to get the wiki summary
-	    get_wiki(lang, text, mic)
+	    self.get_wiki(lang, text, mic)
 
 
-	def get_wiki(lang, text, mic):
+	def get_wiki(self, lang, text, mic):
 	    mic.say(self.gettext("What would you like to learn about?"))
 	    # get the user voice input as string
-	    article_title = mic.activeListen()
+	    article_title = mic.active_listen()
 	    # make a call to the Wikipedia API
-	    request = Request('https://'+lang+'.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles='+article_title)
+	    print 'Making request to : '+'https://'+lang+'.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles='+article_title[0].title()
+	    request = Request('https://'+lang+'.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles='+quote(article_title[0].title()))
 	    try:
+		mic.say(self.gettext("Loading."))
 		response = urlopen(request)
 		data = json.load(response)
 		# Parse the JSON to just get the extract. Always get the first summary.
@@ -41,7 +42,7 @@ class WikiPlugin(plugin.SpeechHandlerPlugin):
 		mic.say(self.gettext("Unable to reach dictionary API."))
 
 
-	def isValid(self, text):
+	def is_valid(self, text):
 	    return any((word.upper() in text.upper()) for word in
                    (self.gettext("WIKI"), self.gettext("WICKY")))
 
